a52e57253b8efbfcad5061d9c38f3159d8cc1802
vibhash.chandra <vibhash.chandra@servicenow.com>
2021-05-23 14:45:40 +0530

    updated

--- quiz/views.py
+++ quiz/views.py
@@ -1,11 +1,4 @@
-import random
-
-from core import settings
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.views.generic.edit import View
 
@@ -14,12 +7,12 @@ from quiz.models import *
 
 class HomeView(View):
     template_name = 'quiz/home.html'
-    
+
     def get(self, request, *args, **kwargs):
         questions_list = []
         questions = Question.objects.all()
         for question in questions:
-            choices = Choice.objects.filter(ques = question)
+            choices = Choice.objects.filter(ques=question)
             questions_list.append({'question': question, 'choices': choices})
         context = {'data': questions_list}
         return render(request, self.template_name, context)
@@ -28,7 +21,7 @@ class HomeView(View):
         questions_list = []
         questions = Question.objects.all()
         for question in questions:
-            choices = Choice.objects.filter(ques = question)
+            choices = Choice.objects.filter(ques=question)
             questions_list.append({'question': question, 'choices': choices})
         context = {'data': questions_list}
         return render(request, self.template_name, context)
